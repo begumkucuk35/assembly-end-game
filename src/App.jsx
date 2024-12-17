f357377a239a317f8685846b798f388f6dcfb244
begumkucuk35 <35begumkucuk@gmail.com>
2024-12-18 00:10:02 +0300

    Header created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-     
-    </>
-  )
+    <main>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>
+          Guess the word within 8 attempts to keep the programming world safe
+          from Assembly!
+        </p>
+      </header>
+    </main>
+  );
 }
 
-export default App
+export default App;
