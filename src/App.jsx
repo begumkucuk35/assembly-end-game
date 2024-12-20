9e5e5b29d36a7c32b72ad0aba9f2b20e8c132350
begum_kucuk9 <begumkucuk9@izmir.bel.tr>
2024-12-20 17:19:11 +0300

    status and language list sections created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import React from "react";
 import "./App.css";
+import { languages } from "./languages";
 
 function App() {
+  const [winningGame, setWinningGame] = React.useState(true);
+
+  const languageElements =languages.map((language, index) => (
+    <span key={index} style={{color: language.color, backgroundColor: language.backgroundColor}}>{language.name}</span>
+  ))
+
   return (
     <main>
       <header>
@@ -10,6 +18,13 @@ function App() {
           from Assembly!
         </p>
       </header>
+      <section className="game-status">
+        <h2>You win!</h2>
+        <p>Well done! 🎉</p>
+      </section>
+      <section className="language-chips">
+          {languageElements}
+      </section>
     </main>
   );
 }
